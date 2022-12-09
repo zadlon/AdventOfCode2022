55b2de78bfe0a5fa1288697dd8f3ecd1c6c8137f
zadlon <zajdlic.maxence@gmail.com>
2022-12-09 19:03:44 +0100

    setup gradle application plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.21"
+    application
 }
 
 group = "org.example"
@@ -23,16 +24,6 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "17"
 }
 
-tasks.register<Jar>("fatJar") {
-    dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources"))
-    archiveClassifier.set("standalone")
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    manifest {
-        attributes("Main-Class" to "AdventOfCodeKt")
-    }
-    val contents = configurations.runtimeClasspath
-        .get()
-        .map { if (it.isDirectory) it else zipTree(it) } + sourceSets.main.get().output
-
-    from(contents)
+application {
+    mainClass.set("AdventOfCodeKt")
 }
